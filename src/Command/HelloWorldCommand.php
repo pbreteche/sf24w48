b4f2bdb6b770525613d228117b7551401d980b31
Pierre Bretéché <pbreteche@dawan.fr>
2024-11-27 13:51:23 +0100

    Console: hello-world command

---
+++ src/Command/HelloWorldCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:hello-world',
+    description: 'Training command',
+)]
+class HelloWorldCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('name', InputArgument::OPTIONAL, 'Tell us your name, so we can say hello to you !', 'World')
+            ->addOption('case', 'c', InputOption::VALUE_REQUIRED, 'Define case transformation. "L" for lowercase, "U" for uppercase')
+            ->setHelp('This command allows you to send hello to you. You can choose a name and specify a case.')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $name = $input->getArgument('name');
+
+        if ($name) {
+            $io->note(sprintf('You passed an argument: %s', $name));
+        }
+
+        if ($c = $input->getOption('case')) {
+            $io->note(sprintf('You passed "case" option: %s', $c));
+            $name = match ($c) {
+                'L' => mb_strtolower($name),
+                'U' => mb_strtoupper($name),
+                default => $name,
+            };
+        }
+
+        $io->success(sprintf('Hello, %s!', $name));
+
+        return Command::SUCCESS;
+    }
+}
