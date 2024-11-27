5e611b1c9fdf8ad620b51d98a73a6166a4e8fd17
Pierre Bretéché <pbreteche@dawan.fr>
2024-11-27 17:15:01 +0100

    Stimulus: controller with targets and action

--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -1,5 +1,12 @@
 import { Controller } from '@hotwired/stimulus';
 
+const MULTIPLIERS = {
+    'px-pt': 3/4,
+    'pt-px': 4/3,
+    'px-in': 1/96,
+    'pt-in': 1/72,
+};
+
 /*
  * This is an example Stimulus controller!
  *
@@ -10,7 +17,9 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+    static targets = ['direction', 'quantity', 'result']
+
+    convert() {
+        this.resultTarget.innerText = MULTIPLIERS[this.directionTarget.value] * this.quantityTarget.value;
     }
 }
