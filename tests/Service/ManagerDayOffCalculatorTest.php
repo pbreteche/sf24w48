32bb3bf5ab3a2da1011434fbfb8c56e8f6920fcf
Pierre Bretéché <pbreteche@dawan.fr>
2024-11-29 11:27:26 +0100

    Mocking service in KernelTestCase

--- tests/Service/ManagerDayOffCalculatorTest.php
+++ tests/Service/ManagerDayOffCalculatorTest.php
@@ -2,6 +2,8 @@
 
 namespace App\Tests\Service;
 
+use App\Model\DateRange;
+use App\Service\Calendar;
 use App\Service\ManagerDayOffCalculator;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
@@ -12,6 +14,16 @@ class ManagerDayOffCalculatorTest extends KernelTestCase
         self::bootKernel();
 
         $container = static::getContainer();
+
+        $mock = $this->createMock(Calendar::class);
+
+        $mock->expects($this->once())
+            ->method('countWorkingDays')
+            ->with($this->isInstanceOf(DateRange::class))
+            ->willReturn(253);
+
+        $container->set(Calendar::class, $mock);
+
         $managerDayOffCalculator = $container->get(ManagerDayOffCalculator::class);
 
         $result = $managerDayOffCalculator->calculate(2024);
