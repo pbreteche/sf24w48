556e3cf0bde900b8aa8aff379daa82a4a786380a
Pierre Bretéché <pbreteche@dawan.fr>
2024-11-29 16:51:14 +0100

    Http client introduction

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,9 +11,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class DefaultController extends AbstractController
 {
@@ -45,5 +49,39 @@ class DefaultController extends AbstractController
         $myDedicatedCache->invalidateTags(['tag-1', 'tag-2']);
 
         return $this->render('default/index.html.twig');
+    }
+
+    #[Route('/http')]
+    public function demoHttpClient(
+        HttpClientInterface $client,
+        DecoderInterface $serializer,
+        CacheInterface $cache,
+        UrlGeneratorInterface $urlGenerator,
+    ): Response {
+        $composteurs = $cache->get('composteurs-quartier-nantes', function (ItemInterface $item) use ($serializer, $client) {
+            $item->expiresAfter(1);
+            $apiEndPoint = 'https://data.nantesmetropole.fr/api/explore/v2.1/catalog/datasets/512042839_composteurs-quartier-nantes-metropole/records';
+            $apiResponse = $client->request('GET', $apiEndPoint, [
+                'query' => [
+                    'limit' => 5,
+                    'where' => 'lieu = "Nantes"',
+                ],
+            ]);
+
+            return $serializer->decode($apiResponse->getContent(), JsonEncoder::FORMAT);
+        });
+
+//        $response = $client->request('GET', $urlGenerator->generate(
+//            'app_article_show',
+//            ['id' => 10],
+//            UrlGeneratorInterface::ABSOLUTE_URL,
+//        ));
+//
+//        dump($response->getContent());
+
+
+        return $this->render('default/http.html.twig', [
+            'composteurs' => $composteurs['results'],
+        ]);
     }
 }
